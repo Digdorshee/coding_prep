231793d622bfbf4e369c8d9511a2875109e42467
Digdorshee Ghosal <digdorsheeghosal@gmail.com>
2024-08-31 10:31:02 +0530

    you dont know is the new change

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ import java.lang.*;
 import java.util.*;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("bye world");
+        System.out.println("you dont know mant things");
     }
 }
