77751119fd0c53636df9b9f70be6a90f92d224d5
Digdorshee Ghosal <digdorsheeghosal@gmail.com>
2024-08-31 10:28:56 +0530

    bye world is now activated

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ import java.lang.*;
 import java.util.*;
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("bye world");
     }
 }
